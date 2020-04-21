47c5255c242fe315d54a0768d3ed7502620874c7
Jonathan Tuta <jonathangetachew@gmail.com>
2020-04-20 20:13:59 -0500

    Firsst Commit: Added project files.

---
+++ src/main/java/com/reactorcourse/reactorcourseapi/ReactorCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.reactorcourse.reactorcourseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorCourseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactorcourse/reactorcourseapi/ReactorCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactorcourse.reactorcourseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
